3d5e6d1631d5b970e41231772f9773ae5ec78119
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-06-19 09:59:09 +0800

    chore: update

--- src/registerInlineCompletionItemProvider.ts
+++ src/registerInlineCompletionItemProvider.ts
@@ -5,7 +5,7 @@ export function registerInlineCompletionItemProvider(callback: (document: vscode
   position: vscode.Position,
   context: vscode.InlineCompletionContext,
   token: vscode.CancellationToken) => ProviderResult<InlineCompletionItem[] | InlineCompletionList>) {
-  vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, {
+  return vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, {
     provideInlineCompletionItems(
       document: vscode.TextDocument,
       position: vscode.Position,
