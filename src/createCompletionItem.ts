baf4d5bf37a4fff451c08f1ec8fe771c6266a8bb
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-06-24 13:40:35 +0800

    chore(createCompletionItem): add snippet param

--- src/createCompletionItem.ts
+++ src/createCompletionItem.ts
@@ -1,6 +1,13 @@
 import * as vscode from 'vscode'
-import type { CompletionItemKind } from 'vscode'
+import type { CompletionItemKind, SnippetString } from 'vscode'
+import { createSnippetString } from './createSnippetString'
 
-export function createCompletionItem(content: string, type?: CompletionItemKind) {
-  return new vscode.CompletionItem(content, type)
+export function createCompletionItem(content: string, snippet?: string | SnippetString, type?: CompletionItemKind) {
+  const completion = new vscode.CompletionItem(content, type)
+  if (snippet !== undefined) {
+    completion.insertText = typeof snippet === 'string'
+      ? createSnippetString(snippet)
+      : snippet
+  }
+  return completion
 }
