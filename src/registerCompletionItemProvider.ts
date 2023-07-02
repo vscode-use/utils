df23002cc3dbf449ba1ec07c4de3d4f4d9460d4e
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-02 14:08:46 +0800

    fix(registerCompletionItemProvider): triggerCharacters not work

--- src/registerCompletionItemProvider.ts
+++ src/registerCompletionItemProvider.ts
@@ -1,11 +1,14 @@
 import * as vscode from 'vscode'
 import type { CompletionItem } from 'vscode'
-export function registerCompletionItemProvider(filter: string | string[], provideCompletionItems: (document: vscode.TextDocument, position: vscode.Position) => CompletionItem[], triggerCharacters: string[] = []) {
+export function registerCompletionItemProvider(filter: string | string[], provideCompletionItems: (document: vscode.TextDocument, position: vscode.Position) => CompletionItem[], triggerCharacters: string | string[]) {
+  if (typeof triggerCharacters === 'string')
+    triggerCharacters = [triggerCharacters]
   return vscode.languages.registerCompletionItemProvider(
     filter,
     {
       provideCompletionItems,
-      ...triggerCharacters,
     },
+    ...triggerCharacters,
   )
 }
+
