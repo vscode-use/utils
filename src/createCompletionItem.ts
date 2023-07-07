168930953ba9376221b7fdee3cb18de20fce35ba
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-08 00:43:56 +0800

    feat(createCompletionItem): add params documentation

--- src/createCompletionItem.ts
+++ src/createCompletionItem.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode'
-import type { CompletionItemKind, SnippetString } from 'vscode'
+import type { CompletionItemKind, MarkdownString, SnippetString } from 'vscode'
 import { createSnippetString } from './createSnippetString'
 
 interface CompletionItemOptions {
@@ -7,15 +7,18 @@ interface CompletionItemOptions {
   snippet?: string | SnippetString
   detail?: string
   type?: CompletionItemKind
+  documentation?: string | MarkdownString
 }
 export function createCompletionItem(options: CompletionItemOptions) {
-  const { content, snippet, detail, type } = options
+  const { content, snippet, detail, type, documentation } = options
   const completion = new vscode.CompletionItem(content, type)
   if (snippet !== undefined) {
     completion.insertText = typeof snippet === 'string'
       ? createSnippetString(snippet)
       : snippet
   }
+  if (documentation)
+    completion.documentation = documentation
   if (detail)
     completion.detail = detail
   return completion
