730e93c009609407eeaa8cb282870488239ae66c
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-09-10 09:48:25 +0800

    chore: openFile改为Promise返回打开文件后的回调

--- src/openFile.ts
+++ src/openFile.ts
@@ -1,7 +1,10 @@
 import * as vscode from 'vscode'
+import type { TextEditor } from 'vscode'
 
-export function openFile(fileUri: string) {
-  vscode.workspace
-    .openTextDocument(fileUri)
-    .then(doc => vscode.window.showTextDocument(doc))
+export function openFile(fileUri: string): Promise<TextEditor> {
+  return new Promise((resolve) => {
+    vscode.workspace
+      .openTextDocument(fileUri)
+      .then(doc => vscode.window.showTextDocument(doc).then(resolve))
+  })
 }
