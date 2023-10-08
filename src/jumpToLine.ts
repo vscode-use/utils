726d8499ea2a9f5d95be566873c0a19c0440f72a
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-10-08 10:50:54 +0800

    feat(jumpToLine): 支持控制跳转后光标的column位置

--- src/jumpToLine.ts
+++ src/jumpToLine.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode'
 import type { TextEditor } from 'vscode'
 import { openFile } from './openFile'
 
-export function jumpToLine(lineNumber: number, filepath: string | TextEditor | undefined = vscode.window.activeTextEditor) {
+export function jumpToLine(lineNumber: number | [number, number], filepath: string | TextEditor | undefined = vscode.window.activeTextEditor) {
   if (!filepath)
     return
   if (typeof filepath === 'string') {
@@ -15,8 +15,12 @@ export function jumpToLine(lineNumber: number, filepath: string | TextEditor | u
   }
 }
 
-export function toLine(lineNumber: number, editor: TextEditor) {
-  const range = new vscode.Range(lineNumber - 1, 0, lineNumber, 0)
+export function toLine(lineNumber: number | [number, number], editor: TextEditor) {
+  let range
+  if (Array.isArray(lineNumber))
+    range = new vscode.Range(lineNumber[0] - 1, lineNumber[1], lineNumber[0], 0)
+  else
+    range = new vscode.Range(lineNumber - 1, 0, lineNumber, 0)
   editor.revealRange(range, vscode.TextEditorRevealType.InCenter)
   editor.selection = new vscode.Selection(range.start, range.start)
 }
