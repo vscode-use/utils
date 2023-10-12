b4938d7b0bbd76d9da6641ceea82c8532d63071d
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-10-12 23:24:30 +0800

    fix(createRange): start和end传参类型必须一样的问题

--- src/createRange.ts
+++ src/createRange.ts
@@ -2,8 +2,12 @@ import * as vscode from 'vscode'
 import type { PositionOption1, PositionOption2 } from './types'
 
 export function createRange(start: PositionOption2 | PositionOption1, end: PositionOption2 | PositionOption1) {
-  return Array.isArray(start) && Array.isArray(end)
-    ? new vscode.Range(new vscode.Position(...start), new vscode.Position(...end))
-    : new vscode.Range(new vscode.Position((start as PositionOption2).line, (start as PositionOption2).character ?? (start as PositionOption2).column!), new vscode.Position((end as PositionOption2).line, (end as PositionOption2).character ?? (end as PositionOption2).column!))
+  const rangeStart = Array.isArray(start)
+    ? new vscode.Position(...start)
+    : new vscode.Position((start as PositionOption2).line, (start as PositionOption2).character ?? (start as PositionOption2).column!)
+  const rangeEnd = Array.isArray(end)
+    ? new vscode.Position(...end)
+    : new vscode.Position((end as PositionOption2).line, (end as PositionOption2).character ?? (end as PositionOption2).column!)
+  return new vscode.Range(rangeStart, rangeEnd)
 }
 
