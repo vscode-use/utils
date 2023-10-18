37711e766070616a76119f9436f7c9c4f12d73dc
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-10-18 09:27:19 +0800

    fix(isInPosition): type

--- src/isInPosition.ts
+++ src/isInPosition.ts
@@ -1,15 +1,16 @@
-import type { RangeLoc } from './types'
+import type { PositionOption2, RangeLoc } from './types'
 
-export function isInPosition(parentLoc: RangeLoc, childLoc: RangeLoc) {
+export function isInPosition(parentLoc: RangeLoc, childLoc: PositionOption2) {
   const { start, end } = parentLoc
-  const { line: startLine } = start
+  const startLine = start.line
   const startcharacter = start.column || start.character
   const endcharacter = end.column || end.character
-  const { line: endLine } = end
-  const { line, character } = childLoc
-  if (line + 1 === startLine && character < startcharacter!)
+  const endLine = end.line
+  const { line } = childLoc
+  const character = childLoc.column || childLoc.character
+  if (line + 1 === startLine && character! < startcharacter!)
     return false
-  if (line + 1 === endLine && character > endcharacter! - 1)
+  if (line + 1 === endLine && character! > endcharacter! - 1)
     return false
   if (line + 1 < startLine)
     return false
