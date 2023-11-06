3228adaff220de6cb93e6f2156a887189311124c
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-06 09:39:30 +0800

    chore(registerCompletionItemProvider): add provideCompletionItems

--- src/registerCompletionItemProvider.ts
+++ src/registerCompletionItemProvider.ts
@@ -1,13 +1,21 @@
 import * as vscode from 'vscode'
-import type { CompletionItem } from 'vscode'
-export function registerCompletionItemProvider(filter: string | string[], provideCompletionItems: (document: vscode.TextDocument, position: vscode.Position) => CompletionItem[], triggerCharacters: string | string[]) {
+import type { CancellationToken, CompletionItem, ProviderResult } from 'vscode'
+
+export function registerCompletionItemProvider<T extends CompletionItem = CompletionItem>(filter: string | string[], provideCompletionItems: (document: vscode.TextDocument, position: vscode.Position) => CompletionItem[], resolveCompletionItem: ((item: T, token: CancellationToken) => ProviderResult<T>) | string | string[], triggerCharacters: string | string[] = []) {
+  if (typeof resolveCompletionItem !== 'function') {
+    triggerCharacters = resolveCompletionItem
+    resolveCompletionItem = ''
+  }
   if (typeof triggerCharacters === 'string')
     triggerCharacters = [triggerCharacters]
   return vscode.languages.registerCompletionItemProvider(
     filter,
-    {
-      provideCompletionItems,
-    },
+    resolveCompletionItem
+      ? {
+          provideCompletionItems,
+          resolveCompletionItem,
+        } as any
+      : { provideCompletionItems },
     ...triggerCharacters,
   )
 }
