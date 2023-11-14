c4f81fe037654da62a0e0a0a12817233eae7c5ba
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-14 15:54:40 +0800

    fix(registerCompletionItemProvider): params type

--- src/registerCompletionItemProvider.ts
+++ src/registerCompletionItemProvider.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode'
-import type { CancellationToken, CompletionItem, ProviderResult } from 'vscode'
+import type { CancellationToken, CompletionItem, CompletionList, ProviderResult } from 'vscode'
 
-export function registerCompletionItemProvider<T extends CompletionItem = CompletionItem>(filter: string | string[], provideCompletionItems: (document: vscode.TextDocument, position: vscode.Position) => CompletionItem[], resolveCompletionItem: ((item: T, token: CancellationToken) => ProviderResult<T>) | string | string[], triggerCharacters: string | string[] = []) {
+export function registerCompletionItemProvider<T extends CompletionItem = CompletionItem>(filter: string | string[], provideCompletionItems: (document: vscode.TextDocument, position: vscode.Position) => ProviderResult<T[] | CompletionList<T>>, resolveCompletionItem: ((item: T, token: CancellationToken) => ProviderResult<T>) | string | string[], triggerCharacters: string | string[] = []) {
   if (typeof resolveCompletionItem !== 'function') {
     triggerCharacters = resolveCompletionItem
     resolveCompletionItem = ''
