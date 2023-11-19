9cfe5c5c6056e73239be7f364af1a7f01576971c
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-19 13:42:00 +0800

    chore(setSelection): add param position to control active

--- src/setSelection.ts
+++ src/setSelection.ts
@@ -2,9 +2,11 @@ import * as vscode from 'vscode'
 import { createRange } from './createRange'
 import type { PositionOption1, PositionOption2 } from './types'
 
-export function setSelection(start: PositionOption2 | PositionOption1, end: PositionOption2 | PositionOption1) {
+export function setSelection(start: PositionOption2 | PositionOption1, end: PositionOption2 | PositionOption1, position: 'left' | 'right' = 'right') {
   const range = createRange(start, end)
-  const selection = new vscode.Selection(range.start, range.end)
+  const selection = position === 'left'
+    ? new vscode.Selection(range.end, range.start)
+    : new vscode.Selection(range.start, range.end)
   const editor = vscode.window.activeTextEditor!
   editor.selection = selection
   editor.revealRange(range)
