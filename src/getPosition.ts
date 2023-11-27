b4b86b298bd8060f1f41ea488b497be688a8764a
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-27 11:47:26 +0800

    chore(getPosition): 支持根据传入的内容计算出位置信息，不传使用当前激活文本

--- src/getPosition.ts
+++ src/getPosition.ts
@@ -1,12 +1,24 @@
 import * as vscode from 'vscode'
+import { getActiveText } from './getActiveText'
 /**
  * 根据offset获取行列
  */
-export function getPosition(offset: number) {
-  const document = vscode.window.activeTextEditor!.document
-  const position = document.positionAt(offset)
-  const line = position.line
-  const column = position.character + 1
+export function getPosition(offset: number, content: string = getActiveText()!) {
+  const contents = content.split('\n')
+  const max = contents.length
+  let num = 0
+  let line = 0
+  let column = 0
+  for (let n = 0; n < max; n++) {
+    const cur = num + contents[n].length + (n > 0 ? 1 : 0)
+    if (num <= offset && cur >= offset) {
+      line = n
+      column = offset - num
+      break
+    }
+
+    num = cur
+  }
   return {
     line,
     column,
