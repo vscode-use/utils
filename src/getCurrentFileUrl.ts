1dd4aa59c93b61bcf448aa171ec6c881129b59d4
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-01-18 15:55:14 +0800

    chore(getCurrentFileUrl): add isUri param

--- src/getCurrentFileUrl.ts
+++ src/getCurrentFileUrl.ts
@@ -4,8 +4,11 @@ import { getActiveTextEditor } from './getActiveTextEditor'
  * 获取当前激活文件的路径
  * @returns
  */
-export function getCurrentFileUrl() {
+export function getCurrentFileUrl(isUri = false) {
   const activeTextEditor = getActiveTextEditor()
-  if (activeTextEditor)
-    return activeTextEditor.document.uri.fsPath
+  if (!activeTextEditor)
+    return
+  return isUri
+    ? activeTextEditor.document.uri
+    : activeTextEditor.document.uri.fsPath
 }
