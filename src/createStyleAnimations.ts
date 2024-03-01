96cb833a7cb6c3defad032d038034ea9885e25e1
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-03-01 10:56:41 +0800

    fix(createStyleAnimations): setStyle

--- src/createStyleAnimations.ts
+++ src/createStyleAnimations.ts
@@ -3,10 +3,6 @@ import { createStyle } from './createStyle'
 import { setStyle } from './setStyle'
 import type { ClearStyle } from './types'
 
-const promiseWrapper = (cb: (resolve: any, reject: any) => void) => {
-  return new Promise(cb)
-}
-
 export async function createStyleAnimations(styles: { style: DecorationRenderOptions; duration?: number; delay?: number }[], options: { duration?: number; range: Range; delay?: number }): Promise<ClearStyle | undefined> {
   let lastClearStyle!: ClearStyle
   for (const s of styles) {
@@ -15,9 +11,9 @@ export async function createStyleAnimations(styles: { style: DecorationRenderOpt
     const style = createStyle(s.style)
     const duration = s.duration ?? options.duration ?? 0
     const delay = s.delay ?? options.delay ?? 0
-    await promiseWrapper(resolve =>
+    await new Promise(resolve =>
       setTimeout(() => {
-        lastClearStyle = setStyle(style)!
+        lastClearStyle = setStyle(style, options.range)!
         setTimeout(resolve, duration)
       }, delay),
     )
