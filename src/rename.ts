9c6bce046749c255757212950b66890dd60f1bf5
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-05-06 21:00:01 +0800

    chore(rename): add options

--- src/rename.ts
+++ src/rename.ts
@@ -4,9 +4,10 @@ import type { Uri } from 'vscode'
 /**
  * 重命名文件
  * @param oldUri 原始文件路径
- * @param newUri  新的文件路径
+ * @param newUri 新的文件路径
+ * @param options { overwrite?: boolean }
  * @returns Promise<void>
  */
-export function rename(oldUri: Uri, newUri: Uri) {
-  return vscode.workspace.fs.rename(oldUri, newUri)
+export function rename(oldUri: Uri, newUri: Uri, options?: { overwrite?: boolean }) {
+  return vscode.workspace.fs.rename(oldUri, newUri, options)
 }
