28e9d0daf8c015a68267a8a4d6d8688f1597b00b
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-07-15 10:51:15 +0800

    fix: type

--- src/createExtension.ts
+++ src/createExtension.ts
@@ -1,6 +1,6 @@
 import type * as vscode from 'vscode'
 
-export function createExtension(activate: (context: vscode.ExtensionContext, disposals: vscode.Disposable[]) => void | { dispose: () => any }[] | Promise<void | { dispose: () => any }[]>, deactivate?: (ext: any) => void) {
+export function createExtension(activate: (context: vscode.ExtensionContext, disposals: vscode.Disposable[]) => void | vscode.Disposable[] | Promise<void | vscode.Disposable[]>, deactivate?: (ext: any) => void) {
   const wrapperActivate = async (context: vscode.ExtensionContext) => {
     const disposals: vscode.Disposable[] = []
     const dispose = await activate(context, disposals)
