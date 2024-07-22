4660f627594f25ee1aee602effc3878d4d3dc0ea
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-07-22 11:48:52 +0800

    fix(createRange): 兼容不同参数

--- src/createRange.ts
+++ src/createRange.ts
@@ -12,24 +12,42 @@ export function createRange(start: PositionOption2 | PositionOption1 | vscode.Po
 export function createRange(start: PositionOption2 | PositionOption1 | number, end: PositionOption2 | PositionOption1 | number, v1?: PositionOption2 | PositionOption1 | number, v2?: number) {
   let _start!: vscode.Position
   let _end!: vscode.Position
-  if (start instanceof vscode.Position)
+  if (start instanceof vscode.Position && end instanceof vscode.Position) {
     _start = start
-  else if (typeof start === 'number' && typeof end === 'number')
-    _start = createPosition(start, end)
-  else if (Array.isArray(start))
-    _start = createPosition(start)
-  else if (typeof start === 'object')
-    _start = createPosition(start.line, start.character ?? start.column!)
-  if (end instanceof vscode.Position)
     _end = end
-  else if (Array.isArray(end))
+  }
+  else if (start instanceof vscode.Position && typeof end === 'number' && typeof v1 === 'number') {
+    _start = start
+    _end = createPosition(end, v1)
+  }
+  else if (start instanceof vscode.Position && Array.isArray(end)) {
+    _start = start
     _end = createPosition(end)
-  else if (typeof v1 === 'number' && typeof v2 === 'number')
+  }
+  else if (typeof start === 'number' && typeof end === 'number' && typeof v1 === 'number' && typeof v2 === 'number') {
+    _start = createPosition(start, end)
     _end = createPosition(v1, v2)
-  else if (Array.isArray(v1))
+  }
+  else if (typeof start === 'number' && typeof end === 'number' && Array.isArray(v1)) {
+    _start = createPosition(start, end)
     _end = createPosition(v1)
-  else if (typeof end === 'object')
-    _end = createPosition(end.line, end.character ?? end.column!)
+  }
+  else if (typeof start === 'number' && typeof end === 'number' && v1 instanceof vscode.Position) {
+    _start = createPosition(start, end)
+    _end = v1
+  }
+  else if (Array.isArray(start) && typeof end === 'number' && typeof v1 === 'number') {
+    _start = createPosition(start)
+    _end = createPosition(end, v1)
+  }
+  else if (Array.isArray(start) && end instanceof vscode.Position) {
+    _start = createPosition(start)
+    _end = end
+  }
+  else if (Array.isArray(start) && Array.isArray(end)) {
+    _start = createPosition(start)
+    _end = createPosition(end)
+  }
 
   if (_start && _end)
     return new vscode.Range(_start, _end)
