f8e449dc89662d4962f3c8b637fc606c78a97fe9
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-08-22 16:49:57 +0800

    fix(isDak): 通过vscode code来判断

--- src/isDark.ts
+++ src/isDark.ts
@@ -2,10 +2,8 @@ import * as vscode from 'vscode'
 
 /**
  * 判断当前主题色
- * 如果主题色不是light就判断是dark
  * @returns boolean
  */
 export function isDark(): boolean {
-  const colorTheme = vscode.workspace.getConfiguration('workbench').get<string>('colorTheme')!
-  return !colorTheme.toLowerCase().includes('light')
+  return vscode.window.activeColorTheme.kind === vscode.ColorThemeKind.Dark
 }
