a4a3288792511a8ac2993f1f4f212a1846b4d291
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-08-27 10:22:18 +0800

    refactor: registerInlineCompletionItemProvider

--- src/registerInlineCompletionItemProvider.ts
+++ src/registerInlineCompletionItemProvider.ts
@@ -3,21 +3,15 @@ import type { InlineCompletionItem, InlineCompletionList, ProviderResult } from 
 
 /**
  * 注册内联自动补全
- * @param callback 回调函数，可返回一个InlineCompletionItem控制显示的内容
+ * @param provideInlineCompletionItems 回调函数，可返回一个InlineCompletionItem控制显示的内容
  * @returns Disposable
  */
-export function registerInlineCompletionItemProvider(callback: (document: vscode.TextDocument,
+export function registerInlineCompletionItemProvider(provideInlineCompletionItems: (document: vscode.TextDocument,
   position: vscode.Position,
   context: vscode.InlineCompletionContext,
-  token: vscode.CancellationToken) => ProviderResult<InlineCompletionItem[] | InlineCompletionList>) {
+  token: vscode.CancellationToken) => ProviderResult<InlineCompletionItem[] | InlineCompletionList>,
+) {
   return vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, {
-    provideInlineCompletionItems(
-      document: vscode.TextDocument,
-      position: vscode.Position,
-      context: vscode.InlineCompletionContext,
-      token: vscode.CancellationToken,
-    ) {
-      return callback(document, position, context, token)
-    },
+    provideInlineCompletionItems,
   })
 }
