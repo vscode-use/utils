abe415dab6bb003f8e896ecad4fe97b2b2330107
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-09-06 11:26:28 +0800

    feat(createPosition): add offsetColumn

--- src/createPosition.ts
+++ src/createPosition.ts
@@ -6,12 +6,12 @@ import type { PositionOption1, PositionOption2 } from './types'
  * @description create position
  * @returns Position
  */
-export function createPosition(pos: number, pos1: number): vscode.Position
-export function createPosition(pos: PositionOption2 | PositionOption1): vscode.Position
-export function createPosition(pos: PositionOption2 | PositionOption1 | number, pos1?: number) {
+export function createPosition(pos: number, pos1: number, offsetLine?: number, offsetColumn?: number): vscode.Position
+export function createPosition(pos: PositionOption2 | PositionOption1, offsetLine?: number, offsetColumn?: number): vscode.Position
+export function createPosition(pos: PositionOption2 | PositionOption1 | number, pos1?: number, offsetLine: number = 0, offsetColumn: number = 0) {
   return Array.isArray(pos)
-    ? new vscode.Position(...pos)
+    ? new vscode.Position(pos[0] - offsetLine, pos[1] - offsetColumn)
     : typeof pos === 'number'
-      ? new vscode.Position(pos, pos1!)
-      : new vscode.Position((pos as PositionOption2).line, (pos as PositionOption2).character ?? (pos as PositionOption2).column!)
+      ? new vscode.Position(pos - offsetLine, pos1! - offsetColumn)
+      : new vscode.Position((pos as PositionOption2).line - (pos1 || 0), ((pos as PositionOption2).character ?? (pos as PositionOption2).column!) - (offsetLine || 0))
 }
