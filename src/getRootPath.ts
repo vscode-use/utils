d5e2b1449a647c777f37f40066e2cc951c3c9f08
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-10-31 12:55:36 +0800

    chore(getRootPath): add isUri

--- src/getRootPath.ts
+++ src/getRootPath.ts
@@ -3,7 +3,10 @@ import { workspace } from 'vscode'
  * 获取当前工作区的根目录
  * @returns 返回当前工作区的根目录
  */
-export function getRootPath() {
-  if (workspace.workspaceFolders)
-    return workspace.workspaceFolders[0].uri.fsPath
+export function getRootPath<T extends boolean = false>(isUri?: T) {
+  if (workspace.workspaceFolders) {
+    return isUri
+      ? workspace.workspaceFolders[0].uri
+      : workspace.workspaceFolders[0].uri.fsPath
+  }
 }
