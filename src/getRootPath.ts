6233c5ce0ec2ab88583a8db11091852043fb4619
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-10-31 12:57:36 +0800

    chore: update type

--- src/getRootPath.ts
+++ src/getRootPath.ts
@@ -1,12 +1,13 @@
+import type { Uri } from 'vscode'
 import { workspace } from 'vscode'
 /**
  * 获取当前工作区的根目录
  * @returns 返回当前工作区的根目录
  */
-export function getRootPath<T extends boolean = false>(isUri?: T) {
+export function getRootPath<T extends boolean = false>(isUri?: T): undefined | (T extends true ? Uri : string) {
   if (workspace.workspaceFolders) {
-    return isUri
+    return (isUri
       ? workspace.workspaceFolders[0].uri
-      : workspace.workspaceFolders[0].uri.fsPath
+      : workspace.workspaceFolders[0].uri.fsPath) as any
   }
 }
