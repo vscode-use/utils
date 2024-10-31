155efebdce6723dcf406ded9bcff88b8abf60fef
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-10-31 13:52:20 +0800

    fix(getRootPath): get current root

--- src/getRootPath.ts
+++ src/getRootPath.ts
@@ -1,13 +1,18 @@
 import type { Uri } from 'vscode'
 import { workspace } from 'vscode'
+import { getActiveTextEditor } from './getActiveTextEditor'
 /**
  * 获取当前工作区的根目录
  * @returns 返回当前工作区的根目录
  */
 export function getRootPath<T extends boolean = false>(isUri?: T): undefined | (T extends true ? Uri : string) {
-  if (workspace.workspaceFolders) {
-    return (isUri
-      ? workspace.workspaceFolders[0].uri
-      : workspace.workspaceFolders[0].uri.fsPath) as any
+  const activeEditor = getActiveTextEditor()
+  if (workspace.workspaceFolders && activeEditor) {
+    const activeFolder = workspace.getWorkspaceFolder(activeEditor.document.uri)
+    if (activeFolder) {
+      return (isUri
+        ? activeFolder.uri
+        : activeFolder.uri.fsPath) as any
+    }
   }
 }
