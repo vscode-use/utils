fbbdabe98132947c29edc3872464699b18fd3c9d
Simon He <57086651+Simon-He95@users.noreply.github.com>
2025-02-10 14:15:59 +0800

    chore: add watchFile & watchFiles

---
+++ src/watchFile.ts
@@ -0,0 +1,21 @@
+import type { GlobPattern } from 'vscode'
+import type { WatchFilesOptions } from './types'
+import { workspace } from 'vscode'
+
+/**
+ * 监听文件变化
+ * @param globPattern 过滤规则
+ * @param options 参数 可监听文件的变化、删除等
+ * @returns 取消监听的函数
+ */
+export function watchFile(globPattern: GlobPattern, options: WatchFilesOptions) {
+  const { onChange, onDelete, ignoreCreateEvents, ignoreChangeEvents, ignoreDeleteEvents } = options
+  const watcher = workspace.createFileSystemWatcher(globPattern, ignoreCreateEvents, ignoreChangeEvents, ignoreDeleteEvents)
+
+  if (onChange)
+    watcher.onDidChange(onChange)
+  if (onDelete)
+    watcher.onDidDelete(onDelete)
+
+  return () => watcher.dispose()
+}
--- src/watchFiles.ts
+++ src/watchFiles.ts
@@ -1,21 +1,16 @@
 import type { GlobPattern } from 'vscode'
 import type { WatchFilesOptions } from './types'
-import { workspace } from 'vscode'
+import { watchFile } from './watchFile'
 
 /**
- * 监听文件变化
- * @param globPattern 过滤规则
+ * 监听多个文件变化
+ * @param globPatterns 过滤规则数组
  * @param options 参数 可监听文件的变化、删除等
  * @returns 取消监听的函数
  */
-export function watchFiles(globPattern: GlobPattern, options: WatchFilesOptions) {
+export function watchFiles(globPatterns: GlobPattern[], options: WatchFilesOptions) {
   const { onChange, onDelete, ignoreCreateEvents, ignoreChangeEvents, ignoreDeleteEvents } = options
-  const watcher = workspace.createFileSystemWatcher(globPattern, ignoreCreateEvents, ignoreChangeEvents, ignoreDeleteEvents)
+  const disposals = globPatterns.map(globPattern => watchFile(globPattern, { onChange, onDelete, ignoreCreateEvents, ignoreChangeEvents, ignoreDeleteEvents }))
 
-  if (onChange)
-    watcher.onDidChange(onChange)
-  if (onDelete)
-    watcher.onDidDelete(onDelete)
-
-  return () => watcher.dispose()
+  return () => disposals.forEach(disposal => disposal())
 }
