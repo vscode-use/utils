293a76168605093b4fb0561bf0ea512337cd13de
Simon He <57086651+Simon-He95@users.noreply.github.com>
2025-02-13 17:07:23 +0800

    fix: prevent returning active text editor for 'exthost' file

--- src/getActiveTextEditor.ts
+++ src/getActiveTextEditor.ts
@@ -3,5 +3,8 @@ import * as vscode from 'vscode'
  * 获取激活的编辑器
  */
 export function getActiveTextEditor() {
-  return vscode.window.activeTextEditor
+  const activeTextEditor = vscode.window.activeTextEditor
+  if (activeTextEditor?.document.fileName === 'exthost')
+    return
+  return activeTextEditor
 }
